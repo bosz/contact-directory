de1f4c2b09a41538328d0835204df217c8d74081
bosz <fongohmartin@gmail.com>
2016-04-05 22:18:23 +0100

    validate signin and relation with xsd. Issue with signup validation

--- processor/pro_auth.php
+++ processor/pro_auth.php
@@ -24,34 +24,52 @@ if (isset($_POST['signup']) || isset($_POST['signin'])) {
 
   /* ================= SIGN IN ===================*/
   if (isset($_POST['signin'])) {
-    $query = 'for $user in doc("users")//user 
-      where 
-        $user/password = "'.$password.'" and 
-        $user[@email="'.$email.'"] 
-      return $user ';
-    if (!sedna_execute($query)) {
-      $status = '<div class="alert alert-danger">Error ' . 
-      sedna_error() .'getting user\'s information</div>';
-    }else {
-      $user = sedna_result_array();
-      if (sizeof($user) == 0 || sizeof($user) > 1) {
-        var_dump($user);
-        $status = '<div class="alert alert-warning"> Wrong username or password. Please, verify your details and try again.</div>';
+
+    $user_xml = '<user><email>'.$email.'</email><password>'.$password.'</password></user>';
+    $xml = new DOMDocument(); 
+    $xml->loadXML($user_xml);
+    /*validate input*/
+    if (!$xml->schemaValidate('xdata/signin.xsd')) {
+
+      $status = '<div class="alert alert-warning">Invalid format for username / password </div>';
+
+    }else{
+
+      $query = 'for $user in doc("users")//user 
+        where 
+          $user/password = "'.$password.'" and 
+          $user[@email="'.$email.'"] 
+        return $user ';
+
+      if (!sedna_execute($query)) {
+
+        $status = '<div class="alert alert-danger">Error ' . 
+        sedna_error() .'getting user\'s information</div>';
+
       }else {
-        /*set session details*/
-        $_SESSION['id'] = md5($email);
-        $_SESSION['email'] = $email;
-        $status = ' <div style="text-align: center;" class="alert alert-success"> Succesfully Logged In </div>';
-        $status .= "<center>You will be redirected to contacts page shortly. If nothing happends, please <a href='contacts.php'> click here. </a>  </center>";
-        ?>
-        <script type="text/javascript">
-          window.setTimeout(function() {
-            location.href = "contacts.php";
-            }, 2000);
-        </script>
-        <?php
-      }
-    }
+
+        $user = sedna_result_array();
+        if (sizeof($user) == 0 || sizeof($user) > 1) {
+          // var_dump($user);
+          $status = '<div class="alert alert-warning"> Wrong username or password. Please, verify your details and try again.</div>';
+
+        }else {
+
+          $_SESSION['id'] = md5($email);
+          $_SESSION['email'] = $email;
+          $status = ' <div style="text-align: center;" class="alert alert-success"> Succesfully Logged In </div>';
+          $status .= "<center>You will be redirected to contacts page shortly. If nothing happends, please <a href='contacts.php'> click here. </a>  </center>";
+          echo '
+          <script type="text/javascript">
+            window.setTimeout(function() {
+              location.href = "contacts.php";
+              }, 2000);
+          </script>';
+
+        }//end of succesfull authentication (session granting)
+      }//end of succesfull query execution
+    }//end of validation test
+
   }else 
 
   /*  ================= SIGN UP =================*/
@@ -75,19 +93,19 @@ if (isset($_POST['signup']) || isset($_POST['signin'])) {
     $user->setAttribute("email", $email);
     $user = $xml->appendChild($user);
 
-    $xml_email = $xml->createElement('email', $email);
-    $xml_password = $xml->createElement('password', $password);
     $xml_firstname = $xml->createElement('first_name', $first_name);
     $xml_lastname = $xml->createElement('last_name', $last_name);
-    $xml_visible = $xml->createElement('visible', $visible);
+    $xml_email = $xml->createElement('email', $email);
     $xml_image = $xml->createElement('image', $image);
+    $xml_password = $xml->createElement('password', $password);
+    $xml_visible = $xml->createElement('visible', $visible);
 
-    $xml_email = $user->appendChild($xml_email);
-    $xml_password = $user->appendChild($xml_password);
     $xml_firstname = $user->appendChild($xml_firstname);
     $xml_lastname = $user->appendChild($xml_lastname);
-    $xml_visible = $user->appendChild($xml_visible);
+    $xml_email = $user->appendChild($xml_email);
     $xml_image = $user->appendChild($xml_image);
+    $xml_password = $user->appendChild($xml_password);
+    $xml_visible = $user->appendChild($xml_visible);
 
     $xml->formatOutput = true;
 
@@ -96,18 +114,32 @@ if (isset($_POST['signup']) || isset($_POST['signin'])) {
        $user_info = $user_info . $xml->saveXML($node);
     }
 
-    /*adding user into users document. look at the sql*/
-    $query = 'UPDATE insert ' . $user_info . ' into doc("users")';
+    /* ================ validate input ====================*/
+    $xml = new DOMDocument(); 
+    $xml->loadXML($user_info);
+    if (!$xml->schemaValidate('xdata/signup.xsd')) {
 
-    /*execute the sql stateuemt to add the user into users*/
-    if (!sedna_execute($query)) {
-      die('Could not add the user\'s information : ' . sedna_error() . "\n");
-    }
+      /*delete image if its fresh upload*/
+      if ($image != 'img/default_user.png') {
+        unlink($image);
+      }
 
-    /*success message to be displayed on the page*/
-    unset($_POST);
-    $status = "<div class='alert alert-success'> $email added succesfull </div> ";
-    
+      $status = '<div class="alert alert-warning">Some fields are invalid, please check and fix them and resubmit your form </div>';
+
+    }else{
+
+      /*adding user into users document. look at the sql*/
+      $query = 'UPDATE insert ' . $user_info . ' into doc("users")';
+
+      /*execute the sql stateuemt to add the user into users*/
+      if (!sedna_execute($query)) {
+        die('Could not add the user\'s information : ' . sedna_error() . "\n");
+      }
+
+      /*success message to be displayed on the page*/
+      unset($_POST);
+      $status = "<div class='alert alert-success'> $email added succesfull </div> ";
+    }//end of xml validation
   }
 }
 
--- processor/pro_relation.php
+++ processor/pro_relation.php
@@ -2,19 +2,31 @@
 
 if(isset($_POST['newRel']))
 {
-
 	$relation = $_POST['relName'];
-	$filename = 'xdata/relation.xml';
-  	$filename = realpath($filename);
+
+	$relation_xml = '<Relations><relation>'.$relation.'</relation></Relations>';
+    $xml = new DOMDocument(); 
+    $xml->loadXML($relation_xml);
+    /*validate input*/
+    if (@(!$xml->schemaValidate('xdata/relation.xsd'))) {
+
+      $status = '<div class="alert alert-warning">Invalid relation name, relation shoudl be between 3 and 100 letters</div>';
+
+    }else{
+
+		$filename = 'xdata/relation.xml';
+	  	$filename = realpath($filename);
 
 
-	$xml = simplexml_load_file($filename);
-	$xml = new SimpleXMLElement($xml->asXML());	
-	$relations = $xml;
-	$relation = $relations->addChild("relation", $relation);
+		$xml = simplexml_load_file($filename);
+		$xml = new SimpleXMLElement($xml->asXML());	
+		$relations = $xml;
+		$relation = $relations->addChild("relation", $relation);
 
-	$xml->asXML($filename);
-	unset($_POST['newRel']);
-	$status = "<div class='alert alert-success'> Relation added succesfull </div> ";
+		$xml->asXML($filename);
+		unset($_POST['newRel']);
+		$status = "<div class='alert alert-success'> Relation added succesfull </div> ";
+
+	}//end of xml validation
 }
 ?>
