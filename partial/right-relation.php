b0964d47db511c92a2c3f40d1a758dd601fb0ede
bosz <fongohmartin@gmail.com>
2016-04-05 11:27:13 +0100

    right relations pane gotten from file

--- partial/right-relation.php
+++ partial/right-relation.php
@@ -1,12 +1,16 @@
 <div>
 	<h2>Relations</h2>
 	<ul class="list-unstyled">
-		<li class="hierachy">Family</li>
-		<li><a href="?relation=father">Father</a><i class="glyphicon glyphicon-remove pull-right"></i></li>
-		<li><a href="?relation=mother">Mother</a><i class="glyphicon glyphicon-remove pull-right"></i></li>
-		<li><a href="?relation=sibling">Sibling</a><i class="glyphicon glyphicon-remove pull-right"></i></li>
-		<li class="hierachy">Business</li>
-		<li><a href="?relation=coworker">Co-worker</a><i class="glyphicon glyphicon-remove pull-right"></i></li>
-		<li><a href="?relation=boss">Boss</a><i class="glyphicon glyphicon-remove pull-right"></i></li>
+		<?php                  
+		    $xml = new DOMDocument;
+		    $xml->load('xdata/relation.xml');
+
+		    $xsl = new DOMDocument;
+		    $xsl->load('xdata/relation.xsl');
+
+		    $proc = new XSLTProcessor;
+		    $proc->importStyleSheet($xsl);
+		    echo $proc->transformToXML($xml);
+		 ?>
 	</ul>
 </div>
